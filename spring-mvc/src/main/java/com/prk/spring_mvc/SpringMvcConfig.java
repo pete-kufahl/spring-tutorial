1da74b67170be007d810ed31350c3de456a88370
Peter Kufahl <pkufahl@gmail.com>
2025-05-25 16:39:17 -0700

    add interceptor for locale change

--- spring-mvc/src/main/java/com/prk/spring_mvc/SpringMvcConfig.java
+++ spring-mvc/src/main/java/com/prk/spring_mvc/SpringMvcConfig.java
@@ -1,10 +1,15 @@
 package com.prk.spring_mvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
@@ -15,12 +20,22 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("/WEB-INF/pdf/");
     }
 
-    // not needed if application.yml configured
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        registry.viewResolver(resolver);
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 }
