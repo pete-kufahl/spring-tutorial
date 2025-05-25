220a27176b198caed5109b9c9e84e5c20b697c30
Peter Kufahl <pkufahl@gmail.com>
2025-05-25 16:25:09 -0700

    resolve static (pdf) files

---
+++ spring-mvc/src/main/java/com/prk/spring_mvc/SpringMvcConfig.java
@@ -0,0 +1,26 @@
+package com.prk.spring_mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
+
+    // not needed if application.yml configured
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        registry.viewResolver(resolver);
+    }
+}
